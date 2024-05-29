8d459ec979ac98ebe1b4df1832809171fa27bded
Nam Hoang Nguyen <35542721+Nam-H-Nguyen@users.noreply.github.com>
2024-05-29 15:00:08 -0700

    Demo form values stored in context api in dashboard screen after account creation

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -1,18 +1,18 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { useUserContext } from '@/context/UserContext'
-import { Heading } from '@gluestack-ui/themed';
+import { Heading, VStack, Text } from '@gluestack-ui/themed';
 
 const Dashboard = () => {
   const { user } = useUserContext();
 
   return (
-    <View>
+    <VStack space='sm'>
       <Heading>Welcome {user.name} 👋</Heading>
       <Text>Email: {user.email}</Text>
+      <Text>Your gender is: {user.gender}</Text>
       <Text>Term accepted?: {user.accepted ? "yes" : "no"}</Text>
       <Text>List of childrens: {JSON.stringify(user.children)}</Text>
-    </View>
+    </VStack>
   )
 }
 
